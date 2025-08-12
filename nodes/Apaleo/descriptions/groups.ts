9d1c692c4440ca900a088ceddbd3802e4b779196
emrekas-softup <emre.kas@softup.co>
2025-08-12 13:20:56 +0200

    Fix/linting issues (#14)
    * refactor: update Apaleo node descriptions and improve error handling
    
    - Changed ESLint rule for 'node-param-description-boolean-without-whether' to 'off'.
    - Fixed URL formatting in ApaleoTrigger node for webhook requests.
    - Enhanced error handling in webhook registration and deletion processes to return false on failure.
    - Updated parameter definitions and resource options for better clarity and consistency across Apaleo nodes.
    
    * 1.0.17

--- nodes/Apaleo/descriptions/groups.ts
+++ nodes/Apaleo/descriptions/groups.ts
@@ -1,16 +1,16 @@
 import { INodeProperties } from 'n8n-workflow';
 
 export const groups = {
-  "availability-v1": "Availability-v1",
-  "booking-v1": "Booking-v1",
-  "finance-v1": "Finance-v1",
-  "inventory-v1": "Inventory-v1",
-  "logs-v1": "Logs-v1",
-  "operations-v1": "Operations-v1",
-  "rateplan-v1": "Rateplan-v1",
-  "reports-v1": "Reports-v1",
-  "settings-v1": "Settings-v1",
-  "webhook-v1": "Webhook-v1"
+	'availability-v1': 'Availability-v1',
+	'booking-v1': 'Booking-v1',
+	'finance-v1': 'Finance-v1',
+	'inventory-v1': 'Inventory-v1',
+	'logs-v1': 'Logs-v1',
+	'operations-v1': 'Operations-v1',
+	'rateplan-v1': 'Rateplan-v1',
+	'reports-v1': 'Reports-v1',
+	'settings-v1': 'Settings-v1',
+	'webhook-v1': 'Webhook-v1',
 };
 
 export const groupOptions: INodeProperties = {
@@ -19,16 +19,16 @@ export const groupOptions: INodeProperties = {
 	type: 'options',
 	noDataExpression: true,
 	options: [
-		{ name: 'Availability-v1', value: 'availability-v1' },
-		{ name: 'Booking-v1', value: 'booking-v1' },
-		{ name: 'Finance-v1', value: 'finance-v1' },
-		{ name: 'Inventory-v1', value: 'inventory-v1' },
-		{ name: 'Logs-v1', value: 'logs-v1' },
-		{ name: 'Operations-v1', value: 'operations-v1' },
-		{ name: 'Rateplan-v1', value: 'rateplan-v1' },
-		{ name: 'Reports-v1', value: 'reports-v1' },
-		{ name: 'Settings-v1', value: 'settings-v1' },
-		{ name: 'Webhook-v1', value: 'webhook-v1' }
+		{ name: 'Availability-V1', value: 'availability-v1' },
+		{ name: 'Booking-V1', value: 'booking-v1' },
+		{ name: 'Finance-V1', value: 'finance-v1' },
+		{ name: 'Inventory-V1', value: 'inventory-v1' },
+		{ name: 'Logs-V1', value: 'logs-v1' },
+		{ name: 'Operations-V1', value: 'operations-v1' },
+		{ name: 'Rateplan-V1', value: 'rateplan-v1' },
+		{ name: 'Reports-V1', value: 'reports-v1' },
+		{ name: 'Settings-V1', value: 'settings-v1' },
+		{ name: 'Webhook-V1', value: 'webhook-v1' },
 	],
 	default: 'availability-v1',
 };
