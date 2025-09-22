0f5c6831fbad6ffd795a3dd8118f4590d4709d2c
emrekas-softup <emre.kas@softup.co>
2025-09-22 22:09:48 +0200

    Add usable as tool, change the authentication (#19)
    * chore: Enhance Apaleo node functionality and update parameter defaults
    
    - Added `usableAsTool` property to the Apaleo node configuration to improve its usability.
    - Updated default values for various parameters in `parameters.ts` to reflect new reservation dates and service details, ensuring accuracy in API interactions.
    - Adjusted JSON structures for reservations and services to align with the latest requirements.
    
    * feat: Revamp ApaleoOAuth2Api credentials with new authentication flow
    
    - Updated the ApaleoOAuth2Api credential class to include Client ID and Client Secret parameters.
    - Removed deprecated parameters and added a session token for improved authentication.
    - Implemented a preAuthentication method to handle token retrieval using client credentials.
    - Updated documentation URL for better reference.
    
    * 1.0.22
    
    * chore: Update package-lock.json with new package name and version changes

--- credentials/ApaleoOAuth2Api.credentials.ts
+++ credentials/ApaleoOAuth2Api.credentials.ts
@@ -1,63 +1,91 @@
-import type { ICredentialType, INodeProperties } from 'n8n-workflow';
+import {
+	ICredentialDataDecryptedObject,
+	ICredentialTestRequest,
+	ICredentialType,
+	IHttpRequestHelper,
+	IHttpRequestOptions,
+	INodeProperties,
+	IAuthenticateGeneric,
+} from 'n8n-workflow';
+import { Buffer } from 'buffer';
 
 export class ApaleoOAuth2Api implements ICredentialType {
 	name = 'apaleoOAuth2Api';
-
-	extends = ['oAuth2Api'];
-
 	displayName = 'Apaleo OAuth2 API';
-
-	documentationUrl =
-		'https://apaleo.dev/guides/oauth-connection/simple-client.html#example-curl-request';
+	documentationUrl = 'https://docs.apaleo.com/';
 
 	properties: INodeProperties[] = [
 		{
-			displayName: 'Grant Type',
-			name: 'grantType',
+			displayName: 'Session Token',
+			name: 'sessionToken',
 			type: 'hidden',
-			default: 'authorizationCode',
+			typeOptions: {
+				expirable: true,
+			},
+			default: '',
 		},
 		{
-			displayName: 'Authorization URL',
-			name: 'authUrl',
-			type: 'hidden',
-			default: 'https://identity.apaleo.com/connect/authorize',
-		},
-		{
-			displayName: 'Access Token URL',
-			name: 'accessTokenUrl',
-			type: 'hidden',
-			default: 'https://identity.apaleo.com/connect/token',
-		},
-		{
-			displayName: 'Authentication',
-			name: 'authentication',
-			type: 'hidden',
-			default: 'body',
-		},
-		{
-			displayName: 'Enabled Scopes',
-			name: 'enabledScopes',
+			displayName: 'Client ID',
+			name: 'clientId',
 			type: 'string',
+			default: '',
 			required: true,
-			default: '',
-			placeholder: 'openid offline_access',
-			description: 'Space-separated list of scopes to request.',
+			description: 'Client ID for the Apaleo API (Custom App)',
 		},
 		{
-			displayName: 'Auth URI Query Parameters',
-			name: 'authQueryParameters',
-			type: 'hidden',
+			displayName: 'Client Secret',
+			name: 'clientSecret',
+			type: 'string',
 			default: '',
-			description:
-				'For some services additional query parameters have to be set which can be defined here',
-			placeholder: '',
-		},
-		{
-			displayName: 'Scope',
-			name: 'scope',
-			type: 'hidden',
-			default: '={{ $self.enabledScopes }}',
+			required: true,
+			description: 'Client Secret for the Apaleo API (Custom App)',
 		},
 	];
+
+	async preAuthentication(this: IHttpRequestHelper, credentials: ICredentialDataDecryptedObject) {
+		const url = `https://identity.apaleo.com/connect/token`;
+		const basicToken = Buffer.from(credentials.clientId + ':' + credentials.clientSecret).toString(
+			'base64',
+		);
+
+		const requestOptions: IHttpRequestOptions = {
+			method: 'POST',
+			url: url,
+			body: {
+				grant_type: 'client_credentials',
+			},
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				Authorization: 'Basic ' + basicToken,
+			},
+		};
+
+		const { access_token, expires_in } = (await this.helpers.httpRequest(requestOptions)) as {
+			access_token: string;
+			expires_in: number;
+		};
+
+		return {
+			sessionToken: access_token,
+			expiresIn: expires_in,
+		};
+	}
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: '=Bearer {{$credentials.sessionToken}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			url: 'https://api.apaleo.com/account/v1/accounts/current',
+			headers: {
+				Authorization: '=Bearer {{$credentials.sessionToken}}',
+			},
+		},
+	};
 }
